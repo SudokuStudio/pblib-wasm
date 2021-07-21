4c5ea4b20d51255404434c762e53f6a868a223f7
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-21 23:12:34 +0000

    Working version

--- api.cpp
+++ api.cpp
@@ -6,19 +6,74 @@ using namespace emscripten;
 
 PB2CNF inst;
 
-std::vector<std::vector<int32_t>> encodeBoth(const std::vector<double>& weights, const std::vector<int32_t>& literals, double leq, double geq, int32_t firstAuxiliaryVariable)
-{
-  std::vector<int64_t> weights64(weights.begin(), weights.end());
+std::vector<int32_t> arrToVecI32(val arr) {
+  return vecFromJSArray<int32_t>(arr);
+}
+
+std::vector<int64_t> arrToVecI64(val arr) {
+  std::vector<double> doubleWeights = vecFromJSArray<double>(arr);
+  std::vector<int64_t> weights(doubleWeights.begin(), doubleWeights.end());
+  return weights;
+}
+
+void appendFormula(val formulaArr, std::vector<std::vector<int32_t>>& formula) {
+  for (std::vector<int32_t> clause : formula) {
+    val clauseArr = val::array();
+    for (int32_t lit : clause) {
+      clauseArr.call<void>("push", val(lit));
+    }
+    formulaArr.call<void>("push", clauseArr);
+  }
+}
+
+int32_t encodeAtMostK(val literalsArr, double k, val formulaArr, int32_t firstAuxiliaryVariable) {
+  std::vector<int32_t> literals = arrToVecI32(literalsArr);
   std::vector<std::vector<int32_t>> formula;
-  inst.encodeBoth(weights64, literals, (int64_t) leq, (int64_t) geq, formula, firstAuxiliaryVariable);
-  return formula;
+  firstAuxiliaryVariable = 1 + inst.encodeAtMostK(literals, (int64_t) k, formula, firstAuxiliaryVariable);
+  appendFormula(formulaArr, formula);
+  return firstAuxiliaryVariable;
+}
+
+int32_t encodeAtLeastK(val literalsArr, double k, val formulaArr, int32_t firstAuxiliaryVariable) {
+  std::vector<int32_t> literals = arrToVecI32(literalsArr);
+  std::vector<std::vector<int32_t>> formula;
+  firstAuxiliaryVariable = 1 + inst.encodeAtLeastK(literals, (int64_t) k, formula, firstAuxiliaryVariable);
+  appendFormula(formulaArr, formula);
+  return firstAuxiliaryVariable;
+}
+
+int32_t encodeLeq(val weightsArr, val literalsArr, double leq, val formulaArr, int32_t firstAuxiliaryVariable) {
+  std::vector<int64_t> weights = arrToVecI64(weightsArr);
+  std::vector<int32_t> literals = arrToVecI32(literalsArr);
+  std::vector<std::vector<int32_t>> formula;
+  firstAuxiliaryVariable = 1 + inst.encodeLeq(weights, literals, (int64_t) leq, formula, firstAuxiliaryVariable);
+  appendFormula(formulaArr, formula);
+  return firstAuxiliaryVariable;
+}
+
+int32_t encodeGeq(val weightsArr, val literalsArr, double geq, val formulaArr, int32_t firstAuxiliaryVariable) {
+  std::vector<int64_t> weights = arrToVecI64(weightsArr);
+  std::vector<int32_t> literals = arrToVecI32(literalsArr);
+  std::vector<std::vector<int32_t>> formula;
+  firstAuxiliaryVariable = 1 + inst.encodeGeq(weights, literals, (int64_t) geq, formula, firstAuxiliaryVariable);
+  appendFormula(formulaArr, formula);
+  return firstAuxiliaryVariable;
+}
+
+int32_t encodeBoth(val weightsArr, val literalsArr, double leq, double geq, val formulaArr, int32_t firstAuxiliaryVariable) {
+  std::vector<int64_t> weights = arrToVecI64(weightsArr);
+  std::vector<int32_t> literals = arrToVecI32(literalsArr);
+  std::vector<std::vector<int32_t>> formula;
+  firstAuxiliaryVariable = 1 + inst.encodeBoth(weights, literals, (int64_t) leq, (int64_t) geq, formula, firstAuxiliaryVariable);
+  appendFormula(formulaArr, formula);
+  return firstAuxiliaryVariable;
 }
 
 // Binding code
 EMSCRIPTEN_BINDINGS(pb2cnf) {
-  register_vector<double>("VectorDouble");
-  register_vector<int32_t>("VectorInt32");
-  register_vector<std::vector<int32_t>>("VectorVectorInt32");
-
+  function("encodeAtMostK", &encodeAtMostK);
+  function("encodeAtLeastK", &encodeAtLeastK);
+  function("encodeLeq", &encodeLeq);
+  function("encodeGeq", &encodeGeq);
   function("encodeBoth", &encodeBoth);
 }
