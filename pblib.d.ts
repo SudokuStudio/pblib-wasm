c2156b701e06a1d39d397a63a12e59e9a6439a94
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-07-27 07:30:57 +0000

    Update with actual typings, 0.0.1

--- pblib.d.ts
+++ pblib.d.ts
@@ -1,2 +1,8 @@
-declare const Module: any;
-export default Module;
+export type Module = {
+    encodeAtMostK(literals: number[], k: number, formula: number[][], firstAuxiliaryVariable: number): number;
+    encodeAtLeastK(literals: number[], k: number, formula: number[][], firstAuxiliaryVariable: number): number;
+    encodeLeq(weights: number[], literals: number[], leq: number, formula: number[][], firstAuxiliaryVariable: number): number;
+    encodeGeq(weights: number[], literals: number[], geq: number, formula: number[][], firstAuxiliaryVariable: number): number;
+    encodeBoth(weights: number[], literals: number[], leq: number, geq:number, formula: number[][], firstAuxiliaryVariable: number): number;
+};
+export default function loadPbLib(): Promise<Module>;
